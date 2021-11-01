5fff6a5debd861248d1e10361adf8356f6457e2a
Andriy Stechak <andriy.stechak@gmail.com>
2021-11-01 19:10:59 +0200

    Fix interface mismatch

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -8,14 +8,22 @@ import (
 )
 
 // HashPassword returns sha512 hash string for provided password
-func HashPassword(password string) string {
+func HashPassword(password string) (string, error) {
 	hash := sha512.New()
-	hash.Write([]byte(password))
+	if _, err := hash.Write([]byte(password)); err != nil {
+		return "", err
+	}
 
-	return fmt.Sprintf("%x", hash.Sum(nil))
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
 }
 
 // CheckPasswordHash checks if provided password is equal to sha512 hash string
+// In case there was error when composing password hash false value will be returned
 func CheckPasswordHash(password, hash string) bool {
-	return HashPassword(password) == hash
+	passwordHash, err := HashPassword(password)
+	if err != nil {
+		return false
+	}
+
+	return passwordHash == hash
 }
