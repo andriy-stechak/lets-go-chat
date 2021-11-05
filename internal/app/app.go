c52aff2a2d8d08a5acbd26cea92367e8b901ecb1
Andriy Stechak <andriy.stechak@gmail.com>
2021-11-05 22:51:53 +0200

    Add health check endpoint

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/andriystech/lgc/internal/app/handlers/monitoring"
 	"github.com/andriystech/lgc/internal/app/handlers/user"
 	"github.com/gorilla/mux"
 )
@@ -12,8 +13,9 @@ const port = ":8080"
 
 func Run() {
 	router := mux.NewRouter()
-	router.HandleFunc("/user/login", user.LogInUserHandler)
-	router.HandleFunc("/user", user.RegisterUserHandler)
+	router.HandleFunc("/user/login", user.LogInUserHandler).Methods("POST")
+	router.HandleFunc("/user", user.RegisterUserHandler).Methods("POST")
+	router.HandleFunc("/_health", monitoring.HealthCheck).Methods("GET")
 	http.Handle("/", router)
 
 	log.Printf("Server is listening %s port", port)
---
+++ internal/app/handlers/monitoring/monitoring.go
@@ -0,0 +1,15 @@
+package monitoring
+
+import (
+	"net/http"
+
+	"github.com/andriystech/lgc/internal/app/handlers/common"
+)
+
+type HealthCheckResult struct {
+	Status string `json:"status"`
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	common.SendJsonResponse(w, HealthCheckResult{Status: "ok"}, http.StatusOK)
+}
