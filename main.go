a5fb19b3c125086c1d4dee979bd3a822262381be
Andriy Stechak <andriy.stechak@gmail.com>
2021-11-16 15:40:34 +0200

    Mongodb integration

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"context"
+	"time"
+
 	"github.com/andriystech/lgc/api/server"
+	"github.com/andriystech/lgc/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	server.Run()
+	serverConfig := config.GetServerConfig()
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Duration(serverConfig.DbConnectionTimeoutInSeconds),
+	)
+	defer cancel()
+	db, err := mongo.Connect(ctx, options.Client().ApplyURI(serverConfig.MongoDbUrl))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Disconnect(ctx)
+	server.Run(db)
 }
